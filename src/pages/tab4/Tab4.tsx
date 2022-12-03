b13ec97dce64f78b91b12e2ef052ac16ccd026ca
GR <madhankumar05072000@protonmail.com>
2022-12-03 16:17:52 +0530

    :zap: Updated dynamic content

--- src/pages/tab4/Tab4.tsx
+++ src/pages/tab4/Tab4.tsx
@@ -1,42 +1,53 @@
 import React from 'react';
 import './Tab4.css';
 import { IonCol, IonGrid, IonRow, IonContent, IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { video } from './data';
 
 export default class Grid extends React.Component {
 
-
-
+    test() {
+        return (
+            video.map((videos, key) => {
+                console.log(key)
+                console.log(videos.name)
+                console.log(videos.src)
+            })
+        );
+    };
+    
     render (){
+        
         return(
             <>
+                {/* {this.test()} */}
                 <IonPage>
                     <IonContent fullscreen>
                         <div className="container">
                             <IonGrid>
                                 <IonRow>
-                                    <IonCol>
-                                        <IonCard>
-                                            {/* <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" /> */}
-                                            <iframe src="https://www.youtube.com/embed/ndi9yMrak8Q" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                                        </IonCard>
-                                    </IonCol>
-                                    <IonCol>
-                                        <IonCard>
-                                            <IonCardHeader>
-                                                <IonCardTitle>Card Title</IonCardTitle>
-                                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-                                            </IonCardHeader>
+                                {video.map((videos, key) => {
+                                    return(
+                                        <> 
+                                            <IonCol>
+                                                <IonCard >
+                                                    <iframe src={videos.src} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
 
-                                            <IonCardContent>
-                                                Here's a small text description for the card content. Nothing more, nothing less.
-                                            </IonCardContent>
-                                        </IonCard>
-                                    </IonCol>
+                                                    <IonCardHeader>
+                                                        <IonCardTitle>{key}</IonCardTitle>
+                                                        <IonCardSubtitle>{videos.name}</IonCardSubtitle>
+                                                    </IonCardHeader>
+                                                </IonCard>
+                                            </IonCol>
+                                        </>
+                                    );
+                                })}
+
                                 </IonRow>
                             </IonGrid>
                         </div>
                     </IonContent>
                 </IonPage>
+                
             </>
         )
     }
