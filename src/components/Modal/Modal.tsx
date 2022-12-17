8506598f66c371762e3c1cb98984d4c9bcfa7d40
GR <madhankumar05072000@protonmail.com>
2022-12-17 12:31:52 +0530

    :zap: icon issue update

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,7 +13,7 @@ const MODAL = (props: any) => {
 
     return (
         <>
-            <IonIcon color="primary" style={{ pointerEvents: 'auto', heigth: '100%' }} icon={create} id={props.element.id} />
+            <IonIcon color="primary" style={{ pointerEvents: 'initial', heigth: '100%' }} icon={create} id={props.element.id} />
             <IonModal id="modal" ref={modal} trigger={props.element.id}>
                 <IonContent>
                     <IonToolbar>
